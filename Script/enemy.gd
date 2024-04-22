dc6834460fc58b360bade7014af8ff18e841d2e8
bezark <johnbezark@gmail.com>
2024-04-22 16:01:09 -0400

    Collision Shape was Disabeled
    It totally was just that the collision shape was disabled, but because we made the enemy local to the level scene, the changes we made to the enemy scene didn't propagate. Deleting the enemy and re-importing it as a reference to the scene fixes the issue.
    
    Now you just have to figure out why it's flying lol.

--- Script/enemy.gd
+++ Script/enemy.gd
@@ -8,6 +8,8 @@ var target = Vector2(0.,0.)
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
+func _ready():
+	print("enemy alert")
 
 func _physics_process(delta):
 	# Add the gravity.
