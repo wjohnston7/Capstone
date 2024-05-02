71a60a9f5d54d55798c6ce6209fab6c96252d9f8
wjohnston7 <157061764+wjohnston7@users.noreply.github.com>
2024-05-01 22:56:13 -0400

    enemy creation?

---
+++ Script/zach_jones.gd
@@ -0,0 +1,36 @@
+extends CharacterBody3D
+
+@onready var navigation = $NavigationAgent3D
+const SPEED = 5.0
+const JUMP_VELOCITY = 4.5
+@export var state = "idle"
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+var target = Vector3(0., 0., 0.,)
+
+func _physics_process(delta):
+	match state:
+		"idle":
+			pass
+		"moving":
+			navigation.target_position = target.position
+			var next_target = navigation.get_next_path_position()
+			velocity = next_target - position
+	
+	
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+	move_and_slide()
+
+
+func _on_area_3d_body_entered(body):
+	if body.is_in_group("player"):
+		state = "moving"
+		target = body
+		navigation.target_position = body.position
+
+
+func _on_area_3d_body_exited(body):
+	if body.is_in_group("player"):
+		state = "idle"
+		velocity = Vector3.ZERO
