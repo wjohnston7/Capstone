ccb957229aff0e01634c854c17b92dd4612c6889
wjohnston7 <157061764+wjohnston7@users.noreply.github.com>
2024-05-01 23:55:05 -0400

    more edits and trying to smooth enemy

--- Script/Ending maker.gd
+++ Script/Ending maker.gd
@@ -6,7 +6,8 @@ extends Node3D
 const start = preload("res://Scene/Freeman_Hall_norm.tscn")
 const ending_bad = preload("res://Scene/freeman_hall_b.tscn")
 const ending_good = preload("res://Scene/freeman_hall_g.tscn")
-
+const bad_txt = preload("res://Script/bad_ending.gd")
+const good_txt = preload("res://Scene/good_ending.tscn")
 func _ready(): 
 	freeman.position = Vector3(0., 0., 0.)
 	add_child(freeman)
@@ -69,25 +70,24 @@ func _on_ending_signal_body_exited(body):
 		$"Ending Signal".queue_free()
 
 
-func _on_ending_timer_timeout():
-	if beer_counter >= 6:
-		get_tree().change_scene_to_file("res://Scene/good_ending.tscn")
-	else:
-		get_tree().change_scene_to_file("res://Scene/bad_ending.tscn")
+#func _on_ending_timer_timeout():
+	#if beer_counter >= 6:
+		#var bad_text = bad_txt.instantiate()
+		#add_child(bad_text)
+	#else:
+		#var good_text = good_txt.instantiate()
+		#add_child(good_text)
 
 
 func _on_ending_signal_2_body_entered(body):
 	if body.is_in_group("player") and beer_counter < 6 or clue_counter < 3:
-		var good_ending = ending_good.instantiate()
-		add_child(good_ending)
-		remove_child(freeman)
-		$"Ending timer".start()
-		print("bad2")
-	elif body.is_in_group("player") and beer_counter >= 6 and clue_counter >= 3:
 		var bad_ending = ending_bad.instantiate()
 		add_child(bad_ending)
 		remove_child(freeman)
 		$"Ending timer".start()
-		print("good2")
-	else:
-		pass
+	elif body.is_in_group("player") and beer_counter >= 6 and clue_counter >= 3:
+		var good_ending = ending_good.instantiate()
+		add_child(good_ending)
+		remove_child(freeman)
+		$"Ending timer".start()
+
--- Script/good_ending.gd
+++ Script/good_ending.gd
@@ -5,8 +5,8 @@ func _on_quit_pressed():
 
 func _on_main_menu_pressed():
 	get_tree().change_scene_to_file("res://Scene/MMenu.tscn")
+
+
 func _input(event):
-	if event is InputEventMouseButton:
-		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
-	elif Input.is_action_just_pressed("cancel"):
+	if Input.is_action_just_pressed("cancel"):
 		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
